e609807c22e6fadfb64bdfa737774c3e43abce41
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-03-27 00:08:12 +0530

    made the home page successfully

--- src/Frontend/components/navbar.jsx
+++ src/Frontend/components/navbar.jsx
@@ -1,10 +1,29 @@
 import '../style/navbar.css'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+// gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(ScrollTrigger)
 const NavBar = () => {
+    useGSAP(()=>{
+        gsap.from('.navbar-container',{
+            duration:1,
+            y:'-5rem',
+            opacity:0,
+            ease:'power3.inOut'
+        })
+        gsap.from('.nb-spn',{
+            duration:2,
+            stagger:0.3,
+            opacity:0,
+            ease:'power3.inOut'
+        })
+    },[])
     return ( 
         <div className="navbar-container">
-            <span>About</span>
-            <span>Create</span>
-            <span>Source Code</span>
+            <span className='nb-spn'>About</span>
+            <span className='nb-spn'>Create</span>
+            <span className='nb-spn'>Source Code</span>
         </div>
      );
 }
