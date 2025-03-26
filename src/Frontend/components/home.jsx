9c9017a40d7728e7575c9faf11291fabca82524a
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-03-26 23:46:50 +0530

    intgreated gsap to the first page

--- src/Frontend/components/home.jsx
+++ src/Frontend/components/home.jsx
@@ -1,11 +1,40 @@
 import '../style/home.css'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(useGSAP)
 const Home = () => {
+    useGSAP(()=>{
+        gsap.from('.detail-container',{
+            xPercent:-100,
+            duration:1,
+            opacity:0,
+            ease:'power3.inOut'
+        })
+        gsap.from('.detail-title',{
+            duration:1.5,
+            opacity:0,
+            xPercent:-100,
+            ease:'power3.inOut'
+        })
+        gsap.from('.detail-description',{
+            duration:2,
+            opacity:0,
+            xPercent:-100,
+            ease:'power3.inOut'
+        })
+        gsap.from('#inpbtn',{
+            duration:2.2,
+            opacity:0,
+            scale:0,
+            ease:'power3'
+        })
+    })
     return ( 
         <div className="home-container">
             <div className="initial-container">
                 <div className="detail-container">
                     <div className="detail-title">Craft Stunning Presentations</div>
-                    <input type="button" value="Get Started" />
+                    <input type="button" value="Get Started" id='inpbtn'/>
                     <div className="detail-description">Welcome to Slide Genie, where creating dynamic presentations is just a few clicks away. Let your ideas shine with our intuitive slide generation tool.</div>
                 </div>
             </div>
