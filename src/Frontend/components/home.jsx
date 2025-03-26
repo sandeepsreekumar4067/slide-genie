cc9ce98c61b0a137a5514c7c1a90c6c1431ebe14
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-03-26 19:34:49 +0530

    minor changes in the home page

--- src/Frontend/components/home.jsx
+++ src/Frontend/components/home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
             <div className="initial-container">
                 <div className="detail-container">
                     <div className="detail-title">Craft Stunning Presentations</div>
+                    <input type="button" value="Get Started" />
                     <div className="detail-description">Welcome to Slide Genie, where creating dynamic presentations is just a few clicks away. Let your ideas shine with our intuitive slide generation tool.</div>
                 </div>
             </div>
@@ -15,6 +16,9 @@ const Home = () => {
                 <div className="second-description">
                 At Slide Genie, we believe in the power of seamless presentation creation. Our AI-powered platform allows users to transform their thoughts into captivating slides effortlessly. Join us on this journey of innovation and creativity.
                 </div>
+            </div>
+            <div className="third-container">
+                
             </div>
         </div>
      );
