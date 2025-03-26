e9d5d9914e87b2a6aedc575568875455da462674
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-03-26 19:20:36 +0530

    made the navbar and made some changes to the css

---
+++ src/Frontend/components/navbar.jsx
@@ -0,0 +1,12 @@
+import '../style/navbar.css'
+const NavBar = () => {
+    return ( 
+        <div className="navbar-container">
+            <span>About</span>
+            <span>Create</span>
+            <span>Source Code</span>
+        </div>
+     );
+}
+ 
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import Home from './Frontend/components/home'
+import NavBar from './Frontend/components/navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <NavBar/>
     <Home />
   </StrictMode>,
 )
