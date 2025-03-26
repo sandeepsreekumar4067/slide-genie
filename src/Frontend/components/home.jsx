c3d6a1b5a7f550b05b4179c7f054d523808a3a3c
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2025-03-26 19:12:04 +0530

    made the home page

--- src/Frontend/components/home.jsx
+++ src/Frontend/components/home.jsx
@@ -3,7 +3,10 @@ const Home = () => {
     return ( 
         <div className="home-container">
             <div className="initial-container">
-                
+                <div className="detail-container">
+                    <div className="detail-title">Craft Stunning Presentations</div>
+                    <div className="detail-description">Welcome to Slide Genie, where creating dynamic presentations is just a few clicks away. Let your ideas shine with our intuitive slide generation tool.</div>
+                </div>
             </div>
         </div>
      );
